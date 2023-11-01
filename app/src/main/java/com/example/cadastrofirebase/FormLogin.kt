aedf7a514b9c5fa628e19cdc1d5bf218f5ea83f3
Silpharny <143123706+Silpharny@users.noreply.github.com>
2023-11-01 18:41:09 -0300

    App v1

---
+++ app/src/main/java/com/example/cadastrofirebase/FormLogin.kt
@@ -0,0 +1,16 @@
+package com.example.cadastrofirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cadastrofirebase.R.layout.activity_form_login
+
+
+class FormLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_form_login)
+
+        
+    }
+}
